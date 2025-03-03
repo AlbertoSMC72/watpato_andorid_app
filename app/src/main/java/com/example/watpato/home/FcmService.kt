f4cc1636b9aa77e37c813850f6a709f3a769a44f
AlbertoSMC72 <albertosmc72@gmail.com>
2025-03-03 13:55:18 -0600

    test firebase

--- app/src/main/java/com/example/watpato/home/FcmService.kt
+++ app/src/main/java/com/example/watpato/home/FcmService.kt
@@ -1,14 +1,17 @@
 package com.example.watpato.home
 
 import android.app.NotificationManager
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.watpato.MyApp
+import com.example.watpato.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FcmService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+        Log.d("FCM", message.notification?.body.toString())
         showNotification(message)
     }
 
@@ -17,7 +20,7 @@ class FcmService : FirebaseMessagingService() {
         val notification = NotificationCompat.Builder(this, MyApp.NOTIFICATION_CHANNEL_ID)
             .setContentTitle(message.notification?.title)
             .setContentText(message.notification?.body)
-//            .setSmallIcon(R.drawable.notification_logo)
+            .setSmallIcon(R.drawable.watpato)
             .setAutoCancel(true)
             .build()
         notificationManager.notify(1, notification)
