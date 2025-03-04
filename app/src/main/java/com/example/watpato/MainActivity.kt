b9d038b388cabd0f7199696f94e852cd33cc45e7
AlbertoSMC72 <albertosmc72@gmail.com>
2025-03-04 12:33:37 -0600

    test firebase

--- app/src/main/java/com/example/watpato/MainActivity.kt
+++ app/src/main/java/com/example/watpato/MainActivity.kt
@@ -1,24 +1,59 @@
 package com.example.watpato
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
 import com.example.watpato.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
+
+    // 1) Aquí declaramos la variable para manejar el resultado de la solicitud de permiso.
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // El usuario concedió el permiso de notificaciones
+                Toast.makeText(this, "Permiso concedido", Toast.LENGTH_SHORT).show()
+                Log.d("dbug", "Permiso de notificaciones concedido")
+            } else {
+                // El usuario denegó el permiso de notificaciones
+                Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                Log.d("dbug", "Permiso de notificaciones denegado")
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val permiso = Manifest.permission.POST_NOTIFICATIONS
+
+            if (ContextCompat.checkSelfPermission(this, permiso) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("dbug", "Permiso de notificación ya concedido")
+            } else {
+                requestPermissionLauncher.launch(permiso)
+            }
+        }
+
         setContent {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = Color.Red,
-                ) {
-                    HomeScreen()
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.Red,
+            ) {
+                HomeScreen()
+            }
         }
     }
 }
--- app/src/main/java/com/example/watpato/MyApp.kt
+++ app/src/main/java/com/example/watpato/MyApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import android.util.Log
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 
@@ -14,25 +15,31 @@ class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        println("App creada")
+
+        createNotificationChannel()
+
         Firebase.messaging.token.addOnCompleteListener {
             if (!it.isSuccessful) {
-                println("El token no fue generado")
+                Log.d("dbug", "Token no fue generado")
+                println("El token no fue generado correctamente")
                 return@addOnCompleteListener
             }
             val token = it.result
-            println("El token es> $token")
+            println("El valor del token es")
+            println(token)
+            Log.d("dbug", token)
         }
-        createNotificationChannel()
     }
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 NOTIFICATION_CHANNEL_ID,
-                "Notificaciones de FCM",
-                NotificationManager.IMPORTANCE_HIGH,
+                "Notificacion de fcm",
+                NotificationManager.IMPORTANCE_HIGH
             )
-            channel.description = "Estas notificaciones van a ser recibidas desde FCM"
+            channel.description = "Esta notificacion va ser recibida desde fcm"
             val notificationManager = getSystemService(NotificationManager::class.java)
             notificationManager.createNotificationChannel(channel)
         }
